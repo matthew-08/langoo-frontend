2499220f102a5f6655768ab9201aedafe547115a
Matthew <matthewjcrosby1@gmail.com>
2023-03-20 13:08:56 +0700

    Added readmes to project breakdown folder

--- src/socket.ts
+++ src/socket.ts
@@ -1,9 +1,9 @@
-import { io } from 'socket.io-client';
-import { apiURL } from './utils/apiUrl';
+import { io } from 'socket.io-client'
+import { apiURL } from './utils/apiUrl'
 
 const socket = io(`${apiURL}`, {
-  autoConnect: false,
-  withCredentials: true,
-});
+    autoConnect: false,
+    withCredentials: true,
+})
 
-export default socket;
+export default socket
