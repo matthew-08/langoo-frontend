c04cfc228a6b5820d2ff0ced21064b7867f5a174
Matthew <matthewjcrosby1@gmail.com>
2023-04-07 09:10:55 +0700

    Change react hook for to have default language value

--- src/pages/homepage/components/RegisterForm.tsx
+++ src/pages/homepage/components/RegisterForm.tsx
@@ -52,6 +52,9 @@ export default function RegisterForm() {
         handleSubmit,
         formState: { errors },
     } = useForm<SignUpForm>({
+        defaultValues: {
+            languages: ['japanese'],
+        },
         resolver: yupResolver(signUpSchema),
     })
     const dispatch = useAppDispatch()
